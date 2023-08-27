40fe1b8af235fa7fb71a456a5382466ac7d568f3
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-08-27 23:01:20 +0200

    Update landing-typewriter.ts

--- resources/ts/special/landing-typewriter.ts
+++ resources/ts/special/landing-typewriter.ts
@@ -1,17 +1,17 @@
 import Typewriter from 'typewriter-effect/dist/core';
 import _ from 'lodash';
 
-let slogans = [
+let slogans_de = [
     'Deutschlands Himmel, Virtuell Perfekt!',
     'Deutschland Fliegt Digital. Steig Ein!',
     'Mit Leidenschaft über den Wolken: VATSIM Germany.',
     'Der Himmel ist Grenzenlos. Unser Service Auch!',
     'Deutschland Online, Deutschland Über den Wolken.',
-    'Hoch Hinaus mit VATSIM Deutschland!',
+    'Hoch Hinaus mit VATSIM Germany!',
     'Virtueller Himmel, Echte Begeisterung.',
     'Im Digitalen Himmel Deutschlands Zu Hause.',
     'Gemeinsam Fliegen, Virtuell Erleben.',
-    'Über Deutschland, Unter Kontrolle: VATSIM.',
+    'Über Deutschland, Unter Kontrolle: VATSIM Germany.',
     'Virtueller Himmel, echte Leidenschaft!',
     'VATSIM Germany: Wo Träume fliegen lernen.',
     'Durch Deutschland fliegen, virtuell verbunden.',
@@ -24,11 +24,43 @@ let slogans = [
     'Deutschlands digitale Flügel, real gesteuert.',
 ];
 
-slogans = _.shuffle(slogans);
+let slogans_en = [
+    "Germany's Sky, Virtually Perfect!",
+    'Germany Flies Digital. Get Onboard!',
+    'With Passion above the Clouds: VATSIM Germany.',
+    "The Sky Knows No Bounds. Our Service Doesn't Either!",
+    'Online in Germany, Above the Clouds.',
+    'Soar High with VATSIM Germany!',
+    'Virtual Sky, Genuine Enthusiasm.',
+    "At Home in Germany's Digital Sky.",
+    'Fly Together, Experience Virtually.',
+    'Above Germany, Under Control: VATSIM.',
+    'Virtual Sky, Real Passion!',
+    'VATSIM Germany: Where Dreams Learn to Fly.',
+    'Fly Through Germany, Virtually Connected.',
+    "Germany's Virtual Skies, Authentically Controlled.",
+    "Fly with Us in Germany's Virtual Sky!",
+    'Real-time, Real Passion, Virtual Sky.',
+    'VATSIM Germany: Where Virtual Flight Becomes Real.',
+    'High Above Germany – Virtually and Passionately.',
+    'For the Love of Flight – VATSIM Germany.',
+    "Germany's Digital Wings, Actively Controlled.",
+    'Take Off into a Virtual World of Realistic Flight Experience – Welcome to VATSIM Germany!',
+    'Fly, Learn, and Grow Together – VATSIM Germany Connects Pilots and Controllers!',
+    'Your Passion for Flying Meets Genuine Teamwork – Become Part of VATSIM Germany!',
+    "Don't Fly Alone – VATSIM Germany Offers You an Active Community of Flight Enthusiasts!",
+    'Experience the Thrill of Virtual Flying in Real Time.',
+];
+
+const metaElement = document.querySelector('meta[name="lang"]');
+const language = metaElement?.getAttribute('content');
+
+let slogans = _.shuffle(language == 'en' ? slogans_en : slogans_de);
 
 new Typewriter('#typewriter', {
     strings: slogans,
     autoStart: true,
     delay: 80,
     deleteSpeed: 20,
+    loop: true,
 });
