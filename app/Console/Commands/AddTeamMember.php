3a246ce810187ea2f87ef01cd53eb7e8527087ef
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-11-12 12:43:56 +0100

    Create AddTeamMember.php

---
+++ app/Console/Commands/AddTeamMember.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Groups\Team;
+use App\Models\Membership\User\User;
+use Illuminate\Console\Command;
+
+class AddTeamMember extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'vatger:add-team-member';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command to the add a member to a team';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user_id = $this->ask('user_id?');
+        $user = User::findOrFail($user_id);
+        $team_name = $this->ask('team_name?');
+        $team = Team::where('name', 'LIKE', $team_name)->firstOrFail();
+        $user->assignRole($team->role);
+    }
+}
