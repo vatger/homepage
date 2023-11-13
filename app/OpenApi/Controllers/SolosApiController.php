7d1d9166d17641b3af636aa228eaf9b984b5e417
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-11-13 10:53:50 +0100

    solos api

---
+++ app/OpenApi/Controllers/SolosApiController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\OpenApi\Controllers;
+
+use App\Models\Groups\Team;
+use App\Models\Membership\TeamspeakRegistration;
+use App\Models\Membership\User\User;
+use App\OpenApi\Helpers\ApiPathfinder;
+use App\OpenApi\SecuritySchemes\TokenSecurityScheme;
+use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
+
+/**
+ *
+ */
+#[OpenApi\PathItem]
+class SolosApiController extends ApiController
+{
+    /**
+     * Solo endorsements member endpoint
+     * @param int $cid the users VATSIM ID
+     */
+    #[OpenApi\Operation(security: TokenSecurityScheme::class)]
+    #[ApiPathfinder('solos.find_member')]
+    public function find_member(int $cid): object
+    {
+        $this->authorizeApiRequest('solos.find_member');
+        $team_a = Team::where('name', 'LIKE', 'ATD Leitung')->firstOrFail()->group;
+        $team_p = Team::where('name', 'LIKE', 'ATD Prüfer')->firstOrFail();
+        $team_w = Team::where('name', 'LIKE', 'EDWW Mentor')->firstOrFail();
+        $team_g = Team::where('name', 'LIKE', 'EDGG Mentor')->firstOrFail();
+        $team_m = Team::where('name', 'LIKE', 'EDMM Mentor')->firstOrFail();
+
+        $user = User::find($cid);
+        $data = new \stdClass();
+        $data->is_vatger_member = !empty($user);
+        $data->is_vatger_atd_lead = $user?->hasRole($team_a);
+        $data->is_vatger_atd_examiner = $user?->hasRole($team_p);
+        $data->is_vatger_mentor = $user?->hasAnyRole($team_w, $team_g, $team_m);
+
+        return $data;
+    }
+}
--- config/forum.php
+++ config/forum.php
@@ -8,7 +8,6 @@ return [
     'apikey' => env('FORUM_API_KEY', ''),
     'newsId' => env('FORUM_NEWS_THREAD', 97),
     'baseGroup' => env('FORUM_BASE_GROUP', 2), // 2 is the standard registered group id by default
-    'memberGroup' => env('FORUM_MEMBER_GROUP', null), // if a user is full member
     'guestGroup' => env('FORUM_GUEST_GROUP', 55), // if a user has no secondary group he gets this group
     'banedGroup' => env('FORUM_BANNED_GROUP', null),
     'inactiveGroup' => env('FORUM_INACTIVE_GROUP', null),
