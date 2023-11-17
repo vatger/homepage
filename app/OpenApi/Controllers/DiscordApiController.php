dd290269ad4ae0accb5f7d12b23a246daa3f8117
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-11-17 22:07:56 +0100

    Update DiscordApiController.php

--- app/OpenApi/Controllers/DiscordApiController.php
+++ app/OpenApi/Controllers/DiscordApiController.php
@@ -28,9 +28,7 @@ class DiscordApiController extends ApiController
         $data->is_vatger_fullmember = $user?->vatgerDetails?->is_vatger_member;
         $data->atc_rating = $user?->vatsimDetails?->rating_atc;
         $data->pilot_rating = $user?->vatsimDetails?->rating_pilot;
-        $data->teams = $user
-            ->teams()
-            ->get()
+        $data->teams = $user->teams
             ->map(fn($team) => $team->name)
             ->values()
             ->toArray();
