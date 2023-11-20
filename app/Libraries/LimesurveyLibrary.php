23c448cf4a58f2bc0d0fc725de15c14d3a39ac85
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-11-20 15:13:11 +0100

    Update LimesurveyLibrary.php

--- app/Libraries/LimesurveyLibrary.php
+++ app/Libraries/LimesurveyLibrary.php
@@ -2,6 +2,8 @@
 
 namespace App\Libraries;
 
+use App\Models\Membership\User\User;
+use App\Models\SurveyKey;
 use Exception;
 use Illuminate\Support\Facades\Log;
 use org\jsonrpcphp\JsonRPCClient;
@@ -20,7 +22,7 @@ class LimesurveyLibrary
         $this->sessionKey = $this->lsJSONRPCClient->get_session_key(config('survey.uname'), config('survey.pass'));
         if (is_array($this->sessionKey)) {
             Log::error(json_encode($this->sessionKey));
-            throw new Exception('LimesurveyLibrary cant get seesion');
+            throw new Exception('LimesurveyLibrary cant get session');
         }
     }
 
@@ -29,9 +31,9 @@ class LimesurveyLibrary
         $this->lsJSONRPCClient->release_session_key($this->sessionKey);
     }
 
-    public function send_req(string $endpoint): object|array|false
+    private function send_req(string $endpoint, ...$data): object|array|false
     {
-        $response = $this->lsJSONRPCClient->list_surveys($this->sessionKey, null);
+        $response = $this->lsJSONRPCClient->$endpoint($this->sessionKey, $data);
 
         if (is_array($response)) {
             $data = json_decode(json_encode($response));
@@ -40,5 +42,37 @@ class LimesurveyLibrary
         }
 
         return $data;
+    }
+
+    //[
+    //  {
+    //      +"sid": 833398,
+    //      +"surveyls_title": "Wahl Leiter FIR München",
+    //      +"startdate": null,
+    //      +"expires": null,
+    //      +"active": "N",
+    //    }
+    //]
+    public function list_surveys(): array
+    {
+        return $this->send_req('list_surveys', null);
+    }
+
+    /**
+     * @param array<User> $users
+     * @return array<SurveyKey>
+     */
+    public function add_participants(int $survey_id, array $users): array
+    {
+        $data = collect($users)
+            ->map(
+                fn(User $u) => (object) [
+                    'email' => $u->email,
+                    'lastname' => $u->lastname,
+                    'firstname' => $u->firstname,
+                ],
+            )
+            ->toArray();
+        return $this->send_req('add_participants', $survey_id, $data, true);
     }
 }
