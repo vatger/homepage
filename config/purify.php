735f6b5df78f543a3ed53f303f90971c34dc0a76
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-11-22 11:03:40 +0100

    Update purify.php

--- config/purify.php
+++ config/purify.php
@@ -1,131 +1,93 @@
 <?php
 
+use Stevebauman\Purify\Definitions\Html5Definition;
+
 return [
     /*
     |--------------------------------------------------------------------------
-    | Settings
+    | Default Config
     |--------------------------------------------------------------------------
     |
-    | The configuration settings array is passed directly to HTMLPurifier.
-    |
-    | Feel free to add / remove / customize these attributes as you wish.
-    |
-    | Documentation: http://htmlpurifier.org/live/configdoc/plain.html
+    | This option defines the default config that is provided to HTMLPurifier.
     |
     */
 
-    'settings' => [
-        /*
-        |--------------------------------------------------------------------------
-        | Core.Encoding
-        |--------------------------------------------------------------------------
-        |
-        | The encoding to convert input to.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#Core.Encoding
-        |
-        */
+    'default' => 'default',
 
-        'Core.Encoding' => 'utf-8',
+    /*
+    |--------------------------------------------------------------------------
+    | Config sets
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure various sets of configuration for differentiated use of HTMLPurifier.
+    | A specific set of configuration can be applied by calling the "config($name)" method on
+    | a Purify instance. Feel free to add/remove/customize these attributes as you wish.
+    |
+    | Documentation: http://htmlpurifier.org/live/configdoc/plain.html
+    |
+    |   Core.Encoding               The encoding to convert input to.
+    |   HTML.Doctype                Doctype to use during filtering.
+    |   HTML.Allowed                The allowed HTML Elements with their allowed attributes.
+    |   HTML.ForbiddenElements      The forbidden HTML elements. Elements that are listed in this
+    |                               string will be removed, however their content will remain.
+    |   CSS.AllowedProperties       The Allowed CSS properties.
+    |   AutoFormat.AutoParagraph    Newlines are converted in to paragraphs whenever possible.
+    |   AutoFormat.RemoveEmpty      Remove empty elements that contribute no semantic information to the document.
+    |
+    */
 
-        /*
-        |--------------------------------------------------------------------------
-        | Core.SerializerPath
-        |--------------------------------------------------------------------------
-        |
-        | The HTML purifier serializer cache path.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#Cache.SerializerPath
-        |
-        */
-
-        'Cache.SerializerPath' => storage_path('app/purify'),
-
-        /*
-        |--------------------------------------------------------------------------
-        | HTML.Doctype
-        |--------------------------------------------------------------------------
-        |
-        | Doctype to use during filtering.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#HTML.Doctype
-        |
-        */
-
-        'HTML.Doctype' => 'XHTML 1.0 Strict',
-
-        /*
-        |--------------------------------------------------------------------------
-        | HTML.Allowed
-        |--------------------------------------------------------------------------
-        |
-        | The allowed HTML Elements with their allowed attributes.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#HTML.Allowed
-        |
-        */
-
-        'HTML.Allowed' => 'h1,h2,h3,h4,h5,h6,b,code,strong,i,em,a[href|title],ul,ol,li,p[style],br,span,img[width|height|alt|src]',
-
-        /*
-        |--------------------------------------------------------------------------
-        | HTML.ForbiddenElements
-        |--------------------------------------------------------------------------
-        |
-        | The forbidden HTML elements. Elements that are listed in
-        | this string will be removed, however their content will remain.
-        |
-        | For example if 'p' is inside the string, the string: '<p>Test</p>',
-        |
-        | Will be cleaned to: 'Test'
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#HTML.ForbiddenElements
-        |
-        */
-
-        'HTML.ForbiddenElements' => '',
-
-        /*
-        |--------------------------------------------------------------------------
-        | CSS.AllowedProperties
-        |--------------------------------------------------------------------------
-        |
-        | The Allowed CSS properties.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#CSS.AllowedProperties
-        |
-        */
-
-        'CSS.AllowedProperties' => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
-
-        /*
-        |--------------------------------------------------------------------------
-        | AutoFormat.AutoParagraph
-        |--------------------------------------------------------------------------
-        |
-        | The Allowed CSS properties.
-        |
-        | This directive turns on auto-paragraphing, where double
-        | newlines are converted in to paragraphs whenever possible.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#AutoFormat.AutoParagraph
-        |
-        */
-
-        'AutoFormat.AutoParagraph' => false,
-
-        /*
-        |--------------------------------------------------------------------------
-        | AutoFormat.RemoveEmpty
-        |--------------------------------------------------------------------------
-        |
-        | When enabled, HTML Purifier will attempt to remove empty
-        | elements that contribute no semantic information to the document.
-        |
-        | http://htmlpurifier.org/live/configdoc/plain.html#AutoFormat.RemoveEmpty
-        |
-        */
-
-        'AutoFormat.RemoveEmpty' => false,
+    'configs' => [
+        'default' => [
+            'Core.Encoding' => 'utf-8',
+            'HTML.Doctype' => 'HTML 4.01 Transitional',
+            'HTML.Allowed' =>
+                'h1,h2,h3,h4,h5,h6,b,code,strong,i,em,s,del,a[href|title],ul,ol,li,p[style],br,span,img[width|height|alt|src],blockquote',
+            'HTML.ForbiddenElements' => '',
+            'CSS.AllowedProperties' =>
+                'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
+            'AutoFormat.AutoParagraph' => false,
+            'AutoFormat.RemoveEmpty' => false,
+        ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | HTMLPurifier definitions
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify a class that augments the HTML definitions used by
+    | HTMLPurifier. Additional HTML5 definitions are provided out of the box.
+    | When specifying a custom class, make sure it implements the interface:
+    |
+    |   \Stevebauman\Purify\Definitions\Definition
+    |
+    | Note that these definitions are applied to every Purifier instance.
+    |
+    | Documentation: http://htmlpurifier.org/docs/enduser-customize.html
+    |
+    */
+
+    'definitions' => Html5Definition::class,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Serializer
+    |--------------------------------------------------------------------------
+    |
+    | The storage implementation where HTMLPurifier can store its serializer files.
+    | If the filesystem cache is in use, the path must be writable through the
+    | storage disk by the web server, otherwise an exception will be thrown.
+    |
+    */
+
+    'serializer' => [
+        'driver' => env('CACHE_DRIVER', 'file'),
+        'cache' => \Stevebauman\Purify\Cache\CacheDefinitionCache::class,
+    ],
+
+    // 'serializer' => [
+    //    'disk' => env('FILESYSTEM_DISK', 'local'),
+    //    'path' => 'purify',
+    //    'cache' => \Stevebauman\Purify\Cache\FilesystemDefinitionCache::class,
+    // ],
 ];
