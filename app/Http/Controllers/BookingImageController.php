41f920cbdebe144c5b36d1aab12738ebbe72180c
Nikolas Görlitz <git@ngoerlitz.de>
2023-11-23 21:43:07 +0100

    add color preference selection

--- app/Http/Controllers/BookingImageController.php
+++ app/Http/Controllers/BookingImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -11,14 +12,20 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class BookingImageController extends Controller
 {
+    private static string $BOOKING_COOKIE_NAME = "vatsim_germany_booking_theme";
+
     public function serveBookingImage(Request $request, string $image_id): Response | JsonResponse | BinaryFileResponse
     {
         if (!Auth::check()) {
+            if ($this->_getDisplayMode($request) == "dark") {
+                return response()->file(storage_path('app/public/booking_image/error_dark.png'));
+            }
+
             return response()->file(storage_path("app/public/booking_image/error.png"));
         }
 
         // http required for internal traffic
-        $url = "http://bookings.vatsim-germany.org/" . $image_id . "/?" . $request->getQueryString();
+        $url = "http://bookings.vatsim-germany.org/" . $image_id . "/?theme=" . $this->_getDisplayMode($request) . "&" . $request->getQueryString();
 
         $response = Http::get($url);
 
@@ -29,5 +36,39 @@ class BookingImageController extends Controller
         }
 
         return response()->json(['error' => 'Not found'], 404);
+    }
+
+    public function setDarkMode(Request $request): JsonResponse
+    {
+        if (!$this->_checkCookieConsent($request)) {
+            return response()->json(['message' => 'Cookie Consent not given. Can\'t save preferences.']);
+        }
+
+        $cookie = cookie(self::$BOOKING_COOKIE_NAME, 'dark', Carbon::now()->addYear()->diffInMinutes(Carbon::now()));
+        return response()->json(['message' => 'Dark Mode Set!'])->withCookie($cookie);
+    }
+
+    public function setLightMode(Request $request): JsonResponse
+    {
+        if (!$this->_checkCookieConsent($request)) {
+            return response()->json(['message' => 'Cookie Consent not given. Can\'t save preferences.']);
+        }
+
+        $cookie = cookie(self::$BOOKING_COOKIE_NAME, 'light', Carbon::now()->addYear()->diffInMinutes(Carbon::now()));
+        return response()->json(['message' => 'Light Mode Set!'])->withCookie($cookie);
+    }
+
+    private function _getDisplayMode(Request $request): string
+    {
+        if ($request->hasCookie(self::$BOOKING_COOKIE_NAME)) {
+            return $request->cookie(self::$BOOKING_COOKIE_NAME);
+        }
+
+        return 'light';
+    }
+
+    private function _checkCookieConsent(Request $request): bool
+    {
+        return $request->cookie('vatger_cookie_consent') != null;
     }
 }
--- routes/web/booking_images.php
+++ routes/web/booking_images.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('booking/image')
     ->group(function() {
+        Route::get("/dark", [BookingImageController::class, 'setDarkMode']);
+        Route::get("/light", [BookingImageController::class, 'setLightMode']);
         Route::get('{image_id}', [BookingImageController::class, 'serveBookingImage']);
     });
