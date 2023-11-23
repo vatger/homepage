874f49ff54b159312a93159085ff3196717a9360
Nikolas Görlitz <git@ngoerlitz.de>
2023-11-23 21:09:02 +0100

    add error image if not logged in

--- app/Http/Controllers/BookingImageController.php
+++ app/Http/Controllers/BookingImageController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class BookingImageController extends Controller
 {
-    public function serveBookingImage(Request $request, string $image_id) {
+    public function serveBookingImage(Request $request, string $image_id): Response | JsonResponse | BinaryFileResponse
+    {
+        if (!Auth::check()) {
+            return response()->file(storage_path("app/public/booking_image/error.png"));
+        }
+
         // http required for internal traffic
         $url = "http://bookings.vatsim-germany.org/" . $image_id . "/?" . $request->getQueryString();
 
--- routes/web/booking_images.php
+++ routes/web/booking_images.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\BookingImageController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('booking/image')
-    ->middleware('auth')
     ->group(function() {
         Route::get('{image_id}', [BookingImageController::class, 'serveBookingImage']);
     });
