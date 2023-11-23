8fa665aa667189faa522557844139bdf7b8238ce
Nikolas Görlitz <git@ngoerlitz.de>
2023-11-23 14:55:46 +0100

    Add query string to allow nextWeek searches

--- app/Http/Controllers/BookingImageController.php
+++ app/Http/Controllers/BookingImageController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class BookingImageController extends Controller
 {
-    public function serveBookingImage(string $image_id) {
-        $response = Http::get("http://bookings.vatsim-germany.org/" . $image_id . "/");
+    public function serveBookingImage(Request $request, string $image_id) {
+        // http required for internal traffic
+        $url = "http://bookings.vatsim-germany.org/" . $image_id . "/?" . $request->getQueryString();
+
+        $response = Http::get($url);
 
         if ($response->successful()) {
             $contentType = $response->header("Content-Type");
@@ -15,6 +19,6 @@ class BookingImageController extends Controller
             return response($response->body())->header('Content-Type', $contentType);
         }
 
-        return response()->json(['error' => 'Image not found'], 404);
+        return response()->json(['error' => 'Not found'], 404);
     }
 }
