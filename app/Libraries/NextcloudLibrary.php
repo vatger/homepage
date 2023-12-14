47e7f9a1564b5cf776494c4d7b7e902df14eb040
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-12-14 18:35:34 +0100

    Update NextcloudLibrary.php

--- app/Libraries/NextcloudLibrary.php
+++ app/Libraries/NextcloudLibrary.php
@@ -91,7 +91,8 @@ class NextcloudLibrary extends BaseLibrary
     {
         $result = self::send('GET', "users/$username/groups");
         $result_data = json_decode(json_encode(simplexml_load_string($result->getBody()->getContents())));
-        $currentgroups = $result_data?->data?->groups?->element ?? [];
+        $result_data_data = $result_data?->data?->groups?->element;
+        $currentgroups = is_array($result_data_data) ? $result_data_data : [$result_data_data];
 
         if (!empty($currentgroups)) {
             $to_delete = array_diff($currentgroups, $newgroups);
