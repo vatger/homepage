6e7642373840e997e587b1e07c8228df4d0b03e1
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2023-12-27 15:10:04 +0100

    Update NextcloudLibrary.php

--- app/Libraries/NextcloudLibrary.php
+++ app/Libraries/NextcloudLibrary.php
@@ -126,8 +126,8 @@ class NextcloudLibrary extends BaseLibrary
         }
 
         foreach ($to_add as $groupadd) {
-            dd($groupadd);
             $result = self::send('POST', "users/$username/groups", ['groupid' => $groupadd]);
+            dd($result);
             if ($result->getStatusCode() != 100) {
                 Log::info("Error member $username could not be added to team $groupadd");
             }
