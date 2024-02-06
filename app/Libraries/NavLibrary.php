12c66f1140e491a9f35cbc9b473545a7dc8aaf55
Sebastian Klietz <mail@sklietz.de>
2024-02-06 11:55:10 +0100

    Fixed VikunjaItem #31 Display GCAP Restricted Stations

--- app/Libraries/NavLibrary.php
+++ app/Libraries/NavLibrary.php
@@ -45,6 +45,8 @@ class NavLibrary extends BaseLibrary
                 $d->setAttribute('ident', $s->logon);
                 $d->setAttribute('frequency', floatval($s?->frequency ?? 199.998));
                 $d->setAttribute('name', $s->description);
+                $d->setAttribute('gcap_class_group', strval($s?->gcap_status ?? '0'));
+                $d->setAttribute('gcap_training_aiport', $s?->gcap_training_airport == false);
                 $d->save();
 
                 // attach to aerodromes
