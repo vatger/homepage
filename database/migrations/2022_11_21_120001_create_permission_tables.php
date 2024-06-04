21ce5daeebe0863d28b1cb4ef98a6828322bdb91
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-06-04 22:28:29 +0200

    fix migrations

--- database/migrations/2022_11_21_120001_create_permission_tables.php
+++ database/migrations/2022_11_21_120001_create_permission_tables.php
@@ -27,7 +27,6 @@ return new class extends Migration {
             //$table->engine('InnoDB');
             $table->bigIncrements('id'); // permission id
             $table->string('name');       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
-            $table->string('type')->nullable();
             $table->string('guard_name'); // For MyISAM use string('guard_name', 25);
             $table->timestamps();
 
@@ -42,6 +41,7 @@ return new class extends Migration {
                 $table->index($columnNames['team_foreign_key'], 'roles_team_foreign_key_index');
             }
             $table->string('name');       // For MyISAM use string('name', 225); // (or 166 for InnoDB with Redundant/Compact row format)
+            $table->string('type')->nullable();
             $table->string('guard_name'); // For MyISAM use string('guard_name', 25);
             $table->timestamps();
             if ($teams || config('permission.testing')) {
