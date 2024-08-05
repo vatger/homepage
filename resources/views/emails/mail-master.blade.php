3c198ee2583372e6d135cb86b3d78edd652b2085
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-08-05 09:29:16 +0200

    Update mail-master.blade.php

--- resources/views/emails/mail-master.blade.php
+++ resources/views/emails/mail-master.blade.php
@@ -10,7 +10,8 @@
 <body style="font-family: Inter, sans-serif; font-size: 15px; font-weight: 400; justify-content: center">
 <!-- Hero Start -->
 <div style="margin-top: 50px;">
-    <table style="font-family: Inter, sans-serif; font-size: 15px; font-weight: 400; max-width: 600px; border: none; margin: 0 auto; border-radius: 6px; overflow: hidden; background-color: #fff; box-shadow: 0 0 3px rgba(60, 72, 88, 0.15);">
+    <table
+        style="font-family: Inter, sans-serif; font-size: 15px; font-weight: 400; max-width: 600px; border: none; margin: 0 auto; border-radius: 6px; overflow: hidden; background-color: #fff; box-shadow: 0 0 3px rgba(60, 72, 88, 0.15);">
         <thead>
         <tr style="background-color: #2f55d4; padding: 3px 0; line-height: 68px; text-align: center; color: #fff; font-size: 24px; font-weight: 700; letter-spacing: 1px;">
             <th scope="col"><img src="{{ asset('images/vacc_logo_white.png') }}" height="48" alt="VATSIM Germany" style="margin-bottom: 10px"></th>
@@ -20,7 +21,8 @@
         <tbody>
         <tr>
             <td style="padding: 24px 24px;">
-                <div style="padding: 8px; color: #000000; background-color: rgba(228, 63, 82, 0.2); border: 1px solid rgba(228, 63, 82, 0.2); border-radius: 6px; text-align: center; font-size: 16px; font-weight: 600;">
+                <div
+                    style="padding: 8px; color: #000000; background-color: rgba(228, 63, 82, 0.2); border: 1px solid rgba(228, 63, 82, 0.2); border-radius: 6px; text-align: center; font-size: 16px; font-weight: 600;">
                     {{ $title }}
                 </div>
             </td>
@@ -61,12 +63,6 @@
         <tr>
             <td style="padding: 16px 8px; color: #8492a6; background-color: #f8f9fc; text-align: center;">
                 Diese automatische Nachricht wurde am {{ \Carbon\Carbon::now()->format('d.m.Y') }} generiert.
-                @if($valid_till)
-                    Sie ist gültig bis {{ $valid_till->format('d.m.Y') }}.
-                @endif
-                @if($delete_at)
-                    Sie wird spätestens am {{ $valid_till->format('d.m.Y') }} gelöscht.
-                @endif
             </td>
         </tr>
         </tbody>
