727f619689df86246d8fa05a0f2f84f51dbcead0
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-09-03 11:40:46 +0200

    Update GithubOauthController.php

--- app/Http/Controllers/GithubOauthController.php
+++ app/Http/Controllers/GithubOauthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Providers\GithubOauthProvider;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
 
 class GithubOauthController extends Controller
