c5a7622a04d3d77098d760106d8fc03329fda00a
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-09-15 18:16:25 +0200

    better inactive emails

--- resources/lang/de/membership_library.php
+++ resources/lang/de/membership_library.php
@@ -4,7 +4,7 @@ return [
     'inactivity_warning' => [
         'title' => 'Dein Vatsim Germany Account - kommende Inaktivität',
         'message' =>
-            'Zum :DATE wird dein Account bei VATSIM Germany auf inaktiv gesetzt. Melde dich in den nächsten 30 Tagen erneut auf der VATSIM Germany Seite an um dies zu verhindern.',
+            'Es wurde festgestellt, dass du dich längere Zeit nicht auf der VATSIM Germany Webseite eingeloggt hast (Hier zu zählen nur Logins auf der Hautseite). Zum :DATE wird dein Account bei VATSIM Germany auf inaktiv gesetzt. Melde dich in den nächsten 30 Tagen erneut auf der VATSIM Germany Seite an um dies zu verhindern.',
         'link' => 'Hier klicken um dich anzumelden',
     ],
     'inactivity_notice' => [
