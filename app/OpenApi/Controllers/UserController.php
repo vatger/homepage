58936a945eefd82cb672f79663022b583fd19303
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2024-11-03 11:24:27 +0100

    message

--- app/OpenApi/Controllers/UserController.php
+++ app/OpenApi/Controllers/UserController.php
@@ -3,6 +3,9 @@
 namespace App\OpenApi\Controllers;
 
 use App\Models\Membership\User;
+use App\Notifications\BasicNotification;
+use App\OpenApi\Helpers\ApiPathfinder;
+use App\OpenApi\Parameters\SendNotificationsParameters;
 use App\OpenApi\Responses\ListUsersResponse;
 use Illuminate\Http\Request;
 use Vyuldashev\LaravelOpenApi\Attributes as OpenApi;
@@ -27,7 +30,29 @@ class UserController extends ApiController
      * Send a notification to the user via board,mail,homepage,...
      */
     #[OpenApi\Operation]
-    public function send_notification(User $cid, Request $request)
+    #[ApiPathfinder('user.send_notification')]
+    #[OpenApi\Parameters(SendNotificationsParameters::class)]
+    public function send_notification(User $cid, Request $request): \Illuminate\Http\JsonResponse
     {
+        $this->authorizeApiRequest('user.send_notification');
+
+        $request->validate([
+            'title' => 'required|string',
+            'message' => 'required|string',
+            'source_name' => 'required|string',
+            'link_text' => 'nullable|string',
+            'link_url' => 'nullable|url',
+        ]);
+
+        $title = $request->input('title');
+        $message = $request->input('message');
+        $source_name = $request->input('source_name');
+        $link_text = $request->input('link_text');
+        $link_url = $request->input('link_url');
+
+        $notification = new BasicNotification($title, $message, $source_name, $link_text, $link_url);
+        $cid->notify($notification);
+
+        return response()->json(['status' => 'Notification sent successfully']);
     }
 }
