650cd92e8d5265c5ed32d0f132617fdd9c0d2c6b
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2025-02-01 14:28:09 +0100

    Update ProcessMembersJob.php

--- app/Jobs/ProcessMembersJob.php
+++ app/Jobs/ProcessMembersJob.php
@@ -35,8 +35,10 @@ class ProcessMembersJob implements ShouldQueue
         Storage::delete($file);
 
         $user = User::find($data->id);
-        CoreApiLibrary2::insertMemberData($user, $data, $time);
-        MembershipLibrary::update($user);
+        if (! empty($user)) {
+            CoreApiLibrary2::insertMemberData($user, $data, $time);
+            MembershipLibrary::update($user);
+        }
 
         if (count($files) > 3) {
             dispatch(new self);
