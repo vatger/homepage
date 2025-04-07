2e918e65cbc08ec87f5384c0eeb1f77c414f6332
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2025-04-07 18:59:50 +0200

    Update head.blade.php

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -14,7 +14,7 @@
 <link rel="icon" type="image/png" href="{{ asset('images/favicon/favicon-96x96.png') }}" sizes="96x96" />
 <link rel="icon" type="image/svg+xml" href="{{ asset('images/favicon/favicon.svg') }}" />
 <link rel="shortcut icon" href="{{ asset('images/favicon/favicon.ico') }}" />
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png.ico') }}" />
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}" />
 <meta name="apple-mobile-web-app-title" content="VATGER" />
 <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}" />
 
