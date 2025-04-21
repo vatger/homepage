e4988dee645e015911ddaaca1707149c738aad46
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2025-04-21 09:48:47 +0200

    gdpr add training

--- config/external_services.php
+++ config/external_services.php
@@ -3,4 +3,5 @@
 return [
     'atciss_gdpr_token' => env('ATCISS_GDPR_TOKEN', ''),
     's1-signup_gdpr_token' => env('S1_GDPR_TOKEN', ''),
+    'training_gdpr_token' => env('TRAINING_GDPR_TOKEN', ''),
 ];
