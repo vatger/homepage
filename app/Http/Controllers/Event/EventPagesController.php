9ba22be51ca1f1e950005c912e2f19ba0e0bec99
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2025-09-14 21:20:12 +0200

    add calender

--- app/Http/Controllers/Event/EventPagesController.php
+++ app/Http/Controllers/Event/EventPagesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Event;
 
 use App\Http\Controllers\Controller;
 use App\Libraries\VATSIM\EventLibrary;
+use Carbon\Carbon;
 
 class EventPagesController extends Controller
 {
@@ -20,5 +21,40 @@ class EventPagesController extends Controller
         }
 
         return view('pages.event')->with(['event' => $event]);
+    }
+
+    public function calendar()
+    {
+        $events = EventLibrary::getGermanEvents(6);
+
+        $html = '<ul class="block-body">';
+        foreach ($events as $event) {
+            $start = Carbon::parse($event->start_time);
+
+            $link = route('events.view', ['id' => $event->id]);
+
+            $html .= '<li class="block-row">';
+            $html .= '<div class="contentRow">';
+            $html .= '<div class="contentRow-figure calendarevents-date-container">';
+            $html .= '<div class="calendarevents-date-container-month">'.$start->getTranslatedShortMonthName().'</div>';
+            $html .= '<div class="calendarevents-date-container-day">'.$start->dayOfMonth.'</div>';
+            $html .= '</div>';
+            $html .= '<div class="contentRow-main contentRow-main--close">';
+            $html .= '<span class="calendarevents-thread-title">';
+            $html .= '<a href="'.$link.'">'.$event->name.'</a>';
+            $html .= '</span>';
+            $html .= '<div class="contentRow-minor contentRow-minor--hideLinks">';
+            // $html .= '<span class="calendarevents-forum-title">';
+            // $html .= '<a href="'.$link.'">'.htmlspecialchars($event['forum_title']).'</a>';
+            // $html .= '</span>';
+            $html .= '</div>'; // contentRow-minor
+            $html .= '</div>'; // contentRow-main
+            $html .= '</div>'; // contentRow
+            $html .= '</li>';
+        }
+        $html .= '</ul>';
+
+        return $html;
+
     }
 }
--- routes/web/events.php
+++ routes/web/events.php
@@ -3,8 +3,14 @@
 use App\Http\Controllers\Event\EventPagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'events', 'as' => 'events.'], function () {
+Route::group(['prefix' => 'events', 'as' => 'events.',  'middleware' => 'cookie.consent'], function () {
 
     Route::get('view/{id}', [EventPagesController::class, 'view'])->name('view');
 
-})->middleware('cookie.consent');
+});
+
+Route::group(['prefix' => 'events', 'as' => 'events.',  'middleware' => 'cookie.redirect'], function () {
+
+    Route::get('calendar', [EventPagesController::class, 'calendar'])->name('calendar');
+
+});
