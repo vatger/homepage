9dde6fa7f9695189b1d4347d3630779bdcf32954
p.hollmann <31701319+paulhollmann@users.noreply.github.com>
2025-10-18 19:44:08 +0200

    tours

--- config/external_services.php
+++ config/external_services.php
@@ -5,4 +5,5 @@ return [
     's1-signup_gdpr_token' => env('S1_GDPR_TOKEN', ''),
     'training_gdpr_token' => env('TRAINING_GDPR_TOKEN', ''),
     'eventmanager_gdpr_token' => env('EVENTMANAGER_GDPR_TOKEN', ''),
+    'tours_gdpr_token' => env('TOURS_GDPR_TOKEN', ''),
 ];
